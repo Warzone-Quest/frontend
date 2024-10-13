2cb1bb962a0080b5a38358884e70ffd61e0cd246
namratarose <namrata.kesar10@gmail.com>
2024-10-13 11:15:39 +0530

    add login and signup page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { Navbar } from "@/components/navbar";
+import './App.css'
 
 const App: React.FC = () => {
   //TODO: conditional rendering
   return (
     <>
-      <div><Outlet /></div>
-      <div className="fixed w-full bottom-0 p-3"><Navbar/></div>
+      <div className="h-screen w-screen"><Outlet /></div>
+      {/* <div className="fixed w-full bottom-0 p-3"><Navbar/></div> */}
     </>
   );
 };
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -5,11 +5,13 @@ import {
 } from "react-router-dom";
 import App from "@/App";
 import Login from "@/pages/Login";
+import SignUpForm from "@/pages/SignUpForm";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route path="login" element={<Login />}></Route>
+      <Route path="signup" element={<SignUpForm />}></Route>
     </Route>,
   ),
 );
